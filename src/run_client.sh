02cbe0630af0b524adf57c0445c21def52bf548d
Victor Nieves Sanchez <vnievess@gmail.com>
2019-06-05 12:04:36 +0200

    commented scripts

--- src/run_client.sh
+++ src/run_client.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+# Move to one client (cliente1)
 cd cliente1
+# Compile the test
 ./compila_test.sh
+
+# Export all needed enviroment variables
 export REGISTRY_HOST=localhost
 export REGISTRY_PORT=10048
-export BLOCKSIZE=5
+export BLOCKSIZE=5 # You can chose this value
+
+# Run the test
 ./ejecuta_test.sh
--- src/run_server.sh
+++ src/run_server.sh
@@ -1,15 +1,28 @@
+#!/bin/bash
+
+# Kill the procces wich is working in 10048 port
+# 10048 is a random port I have choosen
+fuser -k 10048/tcp
+
+# move to the afs directory
 cd afs
 
+# and compile JARS
 ./compila_y_construye_JARS.sh 
+
+# Copy all the JARS in all the clients
 cp afs_* ../cliente1/ 
 cp afs_* ../cliente2/
 cp afs_* ../cliente3/
 cp afs_* ../cliente4/
 
+# Also in the server
 cp afs_* ../servidor/
 
+# Also in src in case it is needed
 cp afs_* ../
 
+# Compile the server and run it
 cd ../servidor
 ./compila_servidor.sh
 ./arranca_rmiregistry 10048 &
